1d4df20d584248cc928a52da9a48716876643d3b
Dmitry Harnitski <dharnitski@gmail.com>
2023-07-16 19:02:52 -0400

    scnWiFi()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include <WiFi.h>
 
 TFT_eSPI tft = TFT_eSPI();
+
+void scnWiFi()
+{
+  Serial.println("scan start");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+  {
+    Serial.println("no networks found");
+  }
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+}
 
 void setup()
 {
@@ -32,9 +64,15 @@ void setup()
   tft.setTextSize(3);
   // We can now plot text on screen using the "print" class
   tft.println("Hello World!");
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  scnWiFi();
 }
 
 void loop()
 {
-
 }
